333d032c9208303a8289f32d6da318a26b77809c
Jacopo <33716357+zagoli@users.noreply.github.com>
2024-05-20 14:36:37 +0200

    Average performers scores queries

--- lib/vota_sanremo/performers.ex
+++ lib/vota_sanremo/performers.ex
@@ -7,6 +7,7 @@ defmodule VotaSanremo.Performers do
   alias VotaSanremo.Repo
 
   alias VotaSanremo.Performers.Performer
+  alias VotaSanremo.Performers.Performer.Queries
 
   @doc """
   Returns the list of performers.
@@ -18,7 +19,7 @@ defmodule VotaSanremo.Performers do
 
   """
   def list_performers do
-    Repo.all(Performer)
+    Queries.all()
   end
 
   @doc """
@@ -100,5 +101,36 @@ defmodule VotaSanremo.Performers do
   """
   def change_performer(%Performer{} = performer, attrs \\ %{}) do
     Performer.changeset(performer, attrs)
+  end
+
+  @doc """
+  Returns the average scores of performers as maps. These maps contain the performer name, the performance type
+  and the average score the performer got in the performance type in the edition corresponding to edition_id.
+
+  ## Examples
+
+      iex> list_performers_avg_score_by_edition(1)
+      [%{name: "Performer_1", performance_type: "Song", score: 12.33},
+      %{name: "Performer_1", performance_type: "Song", score: 12.33}]
+  """
+  def list_performers_avg_score_by_edition(edition_id) when is_integer(edition_id) do
+    Queries.list_performers_avg_score_by_edition(edition_id)
+  end
+
+  @doc """
+  Returns the average weighted scores of performers as maps.
+  The average score is multiplied by the sum of the scores.
+  These maps contain the performer name, the performance type
+  and the weighted average score the performer got in the performance
+  type in the edition corresponding to edition_id.
+
+  ## Examples
+
+      iex> list_performers_weighted_avg_score_by_edition(1)
+      [%{name: "Performer_1", performance_type: "Song", score: 12.33},
+      %{name: "Performer_1", performance_type: "Song", score: 12.33}]
+  """
+  def list_performers_weighted_avg_score_by_edition(edition_id) when is_integer(edition_id) do
+    Queries.list_performers_weighted_avg_score_by_edition(edition_id)
   end
 end
--- test/vota_sanremo/performers_test.exs
+++ test/vota_sanremo/performers_test.exs
@@ -6,7 +6,13 @@ defmodule VotaSanremo.PerformersTest do
   describe "performers" do
     alias VotaSanremo.Performers.Performer
 
-    import VotaSanremo.PerformersFixtures
+    import VotaSanremo.{
+      PerformersFixtures,
+      PerformancesFixtures,
+      VotesFixtures,
+      EditionsFixtures,
+      EveningsFixtures
+    }
 
     @invalid_attrs %{name: nil}
 
@@ -35,7 +41,9 @@ defmodule VotaSanremo.PerformersTest do
       performer = performer_fixture()
       update_attrs = %{name: "some updated name"}
 
-      assert {:ok, %Performer{} = performer} = Performers.update_performer(performer, update_attrs)
+      assert {:ok, %Performer{} = performer} =
+               Performers.update_performer(performer, update_attrs)
+
       assert performer.name == "some updated name"
     end
 
@@ -54,6 +62,95 @@ defmodule VotaSanremo.PerformersTest do
     test "change_performer/1 returns a performer changeset" do
       performer = performer_fixture()
       assert %Ecto.Changeset{} = Performers.change_performer(performer)
+    end
+
+    test "list_performers_avg_score_by_edition/1 lists performers with correct avg score" do
+      scores = 1..10
+
+      {edition_id, performer_name, first_performance_type, second_performance_type} =
+        setup_for_avg_score_tests(scores)
+
+      [first_avg_score, second_avg_score] =
+        Performers.list_performers_avg_score_by_edition(edition_id)
+
+      assert first_avg_score.name == performer_name
+      assert second_avg_score.name == performer_name
+      assert first_avg_score.performance_type in [first_performance_type, second_performance_type]
+
+      assert second_avg_score.performance_type in [
+               first_performance_type,
+               second_performance_type
+             ]
+
+      assert first_avg_score.score == Enum.sum(scores) / Enum.count(scores)
+      assert second_avg_score.score == Enum.sum(scores) / Enum.count(scores)
+    end
+
+    test "list_performers_weighted_avg_score_by_edition/1 lists performers with correct avg score" do
+      scores = 1..10
+
+      {edition_id, performer_name, first_performance_type, second_performance_type} =
+        setup_for_avg_score_tests(scores)
+
+      [first_avg_score, second_avg_score] =
+        Performers.list_performers_weighted_avg_score_by_edition(edition_id)
+
+      assert first_avg_score.name == performer_name
+      assert second_avg_score.name == performer_name
+      assert first_avg_score.performance_type in [first_performance_type, second_performance_type]
+
+      assert second_avg_score.performance_type in [
+               first_performance_type,
+               second_performance_type
+             ]
+
+      assert first_avg_score.score == Enum.sum(scores) / Enum.count(scores) * Enum.sum(scores)
+      assert second_avg_score.score == Enum.sum(scores) / Enum.count(scores) * Enum.sum(scores)
+    end
+
+    @doc """
+    Creates two performances of different type and evenings, and a vote for each performance for each number in the provided range.
+    Returns a tuple with the edition id, performer name, first and second performance type.
+
+    ## Examples
+
+        iex> setup_for_avg_score_test(1..10)
+        {1, "Performer", "Type 1", "Type 2"}
+    """
+    def setup_for_avg_score_tests(scores) do
+      %{id: edition_id} = edition_fixture()
+      %{id: performer_id, name: performer_name} = performer_fixture()
+      %{id: first_performance_type_id, type: first_performance_type} = performance_type_fixture()
+
+      %{id: second_performance_type_id, type: second_performance_type} =
+        performance_type_fixture()
+
+      %{id: first_evening_id} =
+        evening_fixture(%{edition_id: edition_id, date: ~D[2024-12-13], number: 1})
+
+      %{id: second_evening_id} =
+        evening_fixture(%{edition_id: edition_id, date: ~D[2024-12-14], number: 2})
+
+      %{id: first_performance_id} =
+        performance_fixture(%{
+          performer_id: performer_id,
+          evening_id: first_evening_id,
+          performance_type_id: first_performance_type_id
+        })
+
+      %{id: second_performance_id} =
+        performance_fixture(%{
+          performer_id: performer_id,
+          evening_id: second_evening_id,
+          performance_type_id: second_performance_type_id
+        })
+
+      Enum.each(scores, fn score ->
+        vote_fixture(%{score: score, performance_id: first_performance_id})
+        vote_fixture(%{score: score, performance_id: second_performance_id})
+      end)
+
+      {edition_id, performer_name, first_performance_type, second_performance_type}
     end
   end
 end
