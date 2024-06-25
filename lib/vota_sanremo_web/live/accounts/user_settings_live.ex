9cbf6303f440c9ee7904fc1167cc05c6c1dc790a
Jacopo <33716357+zagoli@users.noreply.github.com>
2024-06-25 11:01:07 +0200

    Change user votes privacy in user settings

--- lib/vota_sanremo_web/live/accounts/user_settings_live.ex
+++ lib/vota_sanremo_web/live/accounts/user_settings_live.ex
@@ -3,77 +3,6 @@ defmodule VotaSanremoWeb.UserSettingsLive do
 
   alias VotaSanremo.Accounts
 
-  def render(assigns) do
-    ~H"""
-    <.header class="text-center">
-      Account Settings
-      <:subtitle>Manage your account email address and password settings</:subtitle>
-    </.header>
-
-    <div class="space-y-12 divide-y">
-      <div>
-        <.simple_form
-          for={@email_form}
-          id="email_form"
-          phx-submit="update_email"
-          phx-change="validate_email"
-        >
-          <.input field={@email_form[:email]} type="email" label="Email" required />
-          <.input
-            field={@email_form[:current_password]}
-            name="current_password"
-            id="current_password_for_email"
-            type="password"
-            label="Current password"
-            value={@email_form_current_password}
-            required
-          />
-          <:actions>
-            <.button phx-disable-with="Changing...">Change Email</.button>
-          </:actions>
-        </.simple_form>
-      </div>
-
-      <div>
-        <.simple_form
-          for={@password_form}
-          id="password_form"
-          action={~p"/users/log_in?_action=password_updated"}
-          method="post"
-          phx-change="validate_password"
-          phx-submit="update_password"
-          phx-trigger-action={@trigger_submit}
-        >
-          <input
-            name={@password_form[:email].name}
-            type="hidden"
-            id="hidden_user_email"
-            value={@current_email}
-          />
-          <.input field={@password_form[:password]} type="password" label="New password" required />
-          <.input
-            field={@password_form[:password_confirmation]}
-            type="password"
-            label="Confirm new password"
-          />
-          <.input
-            field={@password_form[:current_password]}
-            name="current_password"
-            type="password"
-            label="Current password"
-            id="current_password_for_password"
-            value={@current_password}
-            required
-          />
-          <:actions>
-            <.button phx-disable-with="Changing...">Change Password</.button>
-          </:actions>
-        </.simple_form>
-      </div>
-    </div>
-    """
-  end
-
   def mount(%{"token" => token}, _session, socket) do
     socket =
       case Accounts.update_user_email(socket.assigns.current_user, token) do
@@ -91,6 +20,7 @@ defmodule VotaSanremoWeb.UserSettingsLive do
     user = socket.assigns.current_user
     email_changeset = Accounts.change_user_email(user)
     password_changeset = Accounts.change_user_password(user)
+    user_changeset = Accounts.change_user(user)
 
     socket =
       socket
@@ -99,6 +29,7 @@ defmodule VotaSanremoWeb.UserSettingsLive do
       |> assign(:current_email, user.email)
       |> assign(:email_form, to_form(email_changeset))
       |> assign(:password_form, to_form(password_changeset))
+      |> assign(:votes_privacy_form, to_form(user_changeset))
       |> assign(:trigger_submit, false)
 
     {:ok, socket}
@@ -163,6 +94,25 @@ defmodule VotaSanremoWeb.UserSettingsLive do
 
       {:error, changeset} ->
         {:noreply, assign(socket, password_form: to_form(changeset))}
+    end
+  end
+
+  def handle_event(
+        "update_votes_privacy",
+        %{"user" => user_params},
+        socket
+      ) do
+    case Accounts.update_user(socket.assigns.current_user, user_params) do
+      {:ok, user} ->
+        votes_privacy_form =
+          user
+          |> Accounts.change_user(user_params)
+          |> to_form()
+
+        {:noreply, assign(socket, :votes_privacy_form, votes_privacy_form)}
+
+      {:error, changeset} ->
+        {:noreply, assign(socket, :votes_privacy_form, to_form(changeset))}
     end
   end
 end
