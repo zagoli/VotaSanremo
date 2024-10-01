a60ac3dbecec7daf799ae0d4bdbe80ae1dbbe01f
Jacopo <33716357+zagoli@users.noreply.github.com>
2024-10-01 15:22:50 +0200

    founded juries on personal juries

--- test/vota_sanremo_web/live/personal_juries_live_test.exs
+++ test/vota_sanremo_web/live/personal_juries_live_test.exs
@@ -10,8 +10,26 @@ defmodule VotaSanremoWeb.PersonalJuriesLiveTest do
     %{jury: jury}
   end
 
+  defp found_jury(%{user: user}) do
+    jury = jury_fixture(founder: user.id)
+    %{founded_jury: jury}
+  end
+
   describe "Personal Juries" do
-    setup [:register_and_log_in_user, :create_jury_for_user]
+    setup [:register_and_log_in_user, :create_jury_for_user, :found_jury]
+
+    test "the page contains the juries founded by the user", %{conn: conn, founded_jury: jury} do
+      {:ok, _live, html} = live(conn, ~p"/juries/personal")
+      assert html =~ "Juries you have founded"
+      assert html =~ "#{jury.name}"
+    end
+
+    test "the page does not show the title of the juries founded by the user if there are none",
+         %{conn: conn, founded_jury: jury} do
+      Juries.delete_jury(jury)
+      {:ok, _live, html} = live(conn, ~p"/juries/personal")
+      refute html =~ "Juries you have founded"
+    end
 
     test "the page contains the juries the user is member of", %{conn: conn, jury: jury} do
       {:ok, _live, html} = live(conn, ~p"/juries/personal")
@@ -19,7 +37,8 @@ defmodule VotaSanremoWeb.PersonalJuriesLiveTest do
       assert html =~ "#{jury.name}"
     end
 
-    test "the page does not show the title of the juries the user is member of if there are none", %{conn: conn, jury: jury, user: user} do
+    test "the page does not show the title of the juries the user is member of if there are none",
+         %{conn: conn, jury: jury, user: user} do
       Juries.remove_member(jury, user)
       {:ok, _live, html} = live(conn, ~p"/juries/personal")
       refute html =~ "Juries you are a member of"
