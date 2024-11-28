914a663d4c7a99830b8f8fb2df079d13d1f09dda
Jacopo Zagoli <jacopo@zagoli.com>
2024-11-28 10:44:55 +0100

    initial test for user invite

---
+++ lib/vota_sanremo_web/live/juries/jury_members_live.ex
@@ -0,0 +1,14 @@
+defmodule VotaSanremoWeb.JuryMembersLive do
+  use VotaSanremoWeb, :live_view
+  alias VotaSanremo.Juries
+
+  def mount(%{"jury_id" => jury_id}, _session, socket) do
+    {:ok,
+     socket
+     |> assign_jury(jury_id)}
+  end
+
+  defp assign_jury(socket, jury_id) do
+    assign(socket, :jury, Juries.get_jury(jury_id))
+  end
+end
---
+++ test/vota_sanremo_web/live/jury_members_live_test.exs
@@ -0,0 +1,43 @@
+defmodule VotaSanremoWeb.NewJuryLiveTest do
+  use VotaSanremoWeb.ConnCase, async: true
+  import Phoenix.LiveViewTest
+  import VotaSanremo.JuriesFixtures
+
+  defp create_jury(%{user: user}) do
+    %{jury: jury_fixture(%{founder: user.id})}
+  end
+
+  defp create_other_user(_) do
+    %{other_user: user_fixture()}
+  end
+
+  describe "Invite member" do
+    setup [:register_and_log_in_user, :create_jury, :create_other_user]
+
+    test "It is possible to invite a member to a jury when founder", %{
+      conn: conn,
+      jury: jury,
+      other_user: other_user
+    } do
+      {:ok, live, _html} = live(conn, ~p"/juries/#{jury.id}/members")
+
+      assert {:ok, live, _html} =
+               live
+               |> element("a", "Add member")
+               |> render_click()
+               |> follow_redirect(conn, ~p"/juries/#{jury.id}/members/invite")
+
+      assert {:ok, _live, html} =
+               live
+               |> element("#user-#{other_user.id} a", "invite")
+               |> render_click()
+               |> follow_redirect(conn, ~p"/juries/#{jury.id}/members")
+
+      assert html =~ "User invited"
+    end
+
+    test "It is not possible to invite a member to a jury when not founder", %{conn: conn} do
+      assert false
+    end
+  end
+end
