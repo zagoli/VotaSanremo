04eb065e00e10e4e53ec931f410555696b1be6d2
Jacopo <33716357+zagoli@users.noreply.github.com>
2025-01-22 09:34:53 +0000

    format

--- lib/vota_sanremo_web/components/info_data_badge.ex
+++ lib/vota_sanremo_web/components/info_data_badge.ex
@@ -12,7 +12,9 @@ defmodule VotaSanremoWeb.InfoDataBadge do
     ~H"""
     <div class="w-24 h-32 p-3 bg-slate-200 rounded-md flex flex-col items-center overflow-hidden">
       <span class="text-slate-700 text-sm font-light text-center">{@label}</span>
-      <span class="text-slate-800 font-semibold mt-2" id={@value_id}>{render_slot(@inner_block)}</span>
+      <span class="text-slate-800 font-semibold mt-2" id={@value_id}>
+        {render_slot(@inner_block)}
+      </span>
     </div>
     """
   end
